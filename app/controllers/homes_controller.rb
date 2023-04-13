f5dc81452f09d473ed98f6b9a17aa06a3abaf6c1
Awais Sajid <awaisror217@gmail.com>
2023-04-13 05:05:04 -0400

    Refector homes index code

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,21 @@
 class HomesController < ApplicationController
-  def index; end
+  def index
+    @cards             = %w[Cash EFT Card Refund].freeze
+    @this_month_cash   = CashUp.where("extract(year from created_at) = ? AND extract(month from created_at) = ?",
+                                      Time.zone.now.year, Time.zone.now.month).sum(:cash)
+    @this_month_eft    = CashUp.where("extract(year from created_at) = ? AND extract(month from created_at) = ?",
+                                      Time.zone.now.year, Time.zone.now.month).sum(:eft)
+    @this_month_card   = CashUp.where("extract(year from created_at) = ? AND extract(month from created_at) = ?",
+                                      Time.zone.now.year, Time.zone.now.month).sum(:card)
+    @this_month_refund = CashUp.where("extract(year from created_at) = ? AND extract(month from created_at) = ?",
+                                      Time.zone.now.year, Time.zone.now.month).sum(:refund)
+
+    this_year_cash_ups = CashUp.where("extract(year from created_at) = ?", Time.zone.now.year)
+    group_by_data      = this_year_cash_ups.group_by { |cashup| cashup.created_at.beginning_of_month }.sort.to_h
+    @results = []
+    group_by_data.each do |date, cash_ups|
+      @results << { month: date.strftime('%b'), sales:  cash_ups.sum(&:cash) + cash_ups.sum(&:card) + cash_ups.sum(&:eft)}
+    end
+    binding.pry
+  end
 end
--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,2 +1,34 @@
 module HomesHelper
+
+  def render_card_image(card)
+    image_tag(image_source(card), alt: card, width: 25)
+  end
+
+  def render_amount(card)
+    case card
+    when 'Cash'
+      @this_month_cash
+    when 'EFT'
+      @this_month_eft
+    when 'Card'
+      @this_month_card
+    else
+      @this_month_refund
+    end
+  end
+
+  private
+
+  def image_source(card)
+    case card
+    when 'Cash'
+      '/assets/rand_vector.svg'
+    when 'EFT'
+      '/assets/eft_vectors.png'
+    when 'Card'
+      '/assets/credit_card.png'
+    else
+      '/assets/refund.svg'
+    end
+  end
 end
