883f5e408a364291d2d509f5b30c5eb450daf6e5
Awais Sajid <awaisror217@gmail.com>
2023-04-13 05:22:37 -0400

    Added total sales

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,8 @@
 class HomesController < ApplicationController
   def index
+    @recent_movement   = []
     @cards             = %w[Cash EFT Card Refund].freeze
+
     @this_month_cash   = CashUp.where("extract(year from created_at) = ? AND extract(month from created_at) = ?",
                                       Time.zone.now.year, Time.zone.now.month).sum(:cash)
     @this_month_eft    = CashUp.where("extract(year from created_at) = ? AND extract(month from created_at) = ?",
@@ -10,12 +12,15 @@ class HomesController < ApplicationController
     @this_month_refund = CashUp.where("extract(year from created_at) = ? AND extract(month from created_at) = ?",
                                       Time.zone.now.year, Time.zone.now.month).sum(:refund)
 
-    this_year_cash_ups = CashUp.where("extract(year from created_at) = ?", Time.zone.now.year)
-    group_by_data      = this_year_cash_ups.group_by { |cashup| cashup.created_at.beginning_of_month }.sort.to_h
-    @results = []
+    group_by_data      = CashUp.this_year.group_by { |cashup| cashup.created_at.beginning_of_month }.sort.to_h
     group_by_data.each do |date, cash_ups|
-      @results << { month: date.strftime('%b'), sales:  cash_ups.sum(&:cash) + cash_ups.sum(&:card) + cash_ups.sum(&:eft)}
+      @recent_movement.push({
+                              month: date.strftime('%b'),
+                              sales: cash_ups.sum(&:cash)+
+                                     cash_ups.sum(&:card) +
+                                     cash_ups.sum(&:eft)
+                            })
     end
-    binding.pry
+
   end
 end
--- app/models/cash_up.rb
+++ app/models/cash_up.rb
@@ -1,2 +1,3 @@
 class CashUp < ApplicationRecord
+  scope :this_year, -> { where("extract(year from created_at) = ?", Time.zone.now.year) }
 end
