d80e29bcc009bb8a48afb2e0eabc8a85c306c868
Awais Sajid <awaisror217@gmail.com>
2023-05-05 06:55:13 -0400

    Added comment to custom queries

--- app/models/cash_up.rb
+++ app/models/cash_up.rb
@@ -1,4 +1,6 @@
 class CashUp < ApplicationRecord
+
+  # Custom Queries
   scope :this_year, -> { where("extract(year from created_at) = ?", Time.zone.now.year) }
-  scope :between, ->(start_date, end_date) { where(created_at: start_date..end_date) }
+  scope :between, ->(start_date, end_date) { where(cash_up_date: start_date..end_date) }
 end
