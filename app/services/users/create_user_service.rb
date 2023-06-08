b9084a467ea6b8da3fc3fbd0a2ec413be57fc1b7
Awais Sajid <awaisror217@gmail.com>
2023-06-08 06:35:12 -0400

    service: Set office worker for new role

--- app/services/users/create_user_service.rb
+++ app/services/users/create_user_service.rb
@@ -6,12 +6,11 @@ module Users
 
     def call
       user          = User.new(params)
+      user.role     = 4 if params[:role].present? && params[:role] == 'Office worker'
       user.email    = Faker::Internet.email
       user.password = Faker::Internet.password(min_length: 6)
 
       user.save
-
-
     end
 
     private
